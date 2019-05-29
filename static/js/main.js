08caf06069b61a3ba07dec226141d37e285a8fb3
Mikayla Robinson <mikayla22robinson@gmail.com>
2019-05-28 21:55:01 -0400

    Initial commit with work in progress

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  $("#my_nav_bar").toggleClass("responsive");
+}
+
+$(document).ready(function(){
+  $(".about_the_website").fadeIn();
+});
