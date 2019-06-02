4442c4ab5327d094f56cf71b947e6088adc4a358
Mikayla Robinson <mikayla22robinson@gmail.com>
2019-06-01 21:34:35 -0400

    reorganized as a Flask app

---
+++ FlaskApp/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+@app.route("/about_me")
+def about_page():
+    return render_template("about.html")
+
+@app.route("/learning_topics")
+def topics_page():
+    return render_template("topics.html")
+
+@app.route("/side_projects")
+def projects_page():
+    return render_template("projects.html")
+
+
+
+if __name__ == "__main__":
+    app.run()
