2f7a9cfdf1a888529f916ceb06d679124d8cd3f6
Mikayla Robinson <mikayla22robinson@gmail.com>
2019-06-08 22:54:20 -0400

    refactoring from pull request

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,14 +1,13 @@
-import os
 import dotenv
+import os
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_login import current_user, login_user, LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import func
-from flask import Flask, render_template, redirect, url_for
 from flask_wtf import FlaskForm
+from sqlalchemy.sql import func
+from werkzeug.security import generate_password_hash, check_password_hash
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
-from flask_login import current_user, login_user, LoginManager
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask import flash
 
 app = Flask(__name__)
 
@@ -68,7 +67,7 @@ class User(db.Model):
     is_admin = db.Column(db.Boolean)
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)    
+        return "<User {}>".format(self.username)    
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -80,23 +79,29 @@ class User(db.Model):
         self.is_admin = is_admin
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
     password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    secret_pass = PasswordField('Entry Code', validators=[DataRequired()])
-    submit = SubmitField('Register')
+        "Repeat Password", validators=[DataRequired(), EqualTo("password")])
+    secret_pass = PasswordField("Entry Code", validators=[DataRequired()])
+    submit = SubmitField("Register")
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError('Please use a different username.')
+            raise ValidationError("Please use a different username.")
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError('Please use a different email address.')
+            raise ValidationError("Please use a different email address.")
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
 
 
 if os.environ.get("CREATE_DATABASE"):
@@ -119,17 +124,17 @@ def topics_page():
 def projects_page():
     return render_template("projects.html")
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     form = RegistrationForm()
     if form.validate_on_submit():
+        # Use secret password to prevent unwanted registrations
         if form.secret_pass != os.environ.get("SECRET_PASSWORD"):
-            flash("I don't think you should be doing this.")
-            return redirect(url_for('home_page'))
+            flash("I don"t think you should be doing this.")
+            return redirect(url_for("home_page"))
             
-
         user = User(username=form.username.data, email=form.email.data)
 
         if form.username.data == "mikayla":
@@ -138,9 +143,14 @@ def register():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('home_page'))
-    return render_template('register.html', title='Register', form=form)
+        flash("Congratulations, you are now a registered user!")
+        return redirect(url_for("home_page"))
+    return render_template("register.html", title="Register", form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Sign In", form=form)
 
 if __name__ == "__main__":
     app.run()
