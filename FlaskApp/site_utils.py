6ce974f8f80d48f69add88783dd7f57dea716ddb
Mikayla Robinson <mikayla22robinson@gmail.com>
2019-06-09 20:23:57 -0400

    Add blgo posts

---
+++ FlaskApp/site_utils.py
@@ -0,0 +1,9 @@
+import string
+
+def make_url_slug(title):
+    url_slug = title.lower().strip()
+    exclude = set(string.punctuation)
+    url_slug = "".join(ch for ch in url_slug if ch not in exclude)
+    url_slug = url_slug.replace(" ", "-")
+    return url_slug
+
